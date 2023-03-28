c6c69eddfe685947a28ea7887eb8955792cbfc61
Jacoby Johnson <jacobyajohnson@gmail.com>
2023-03-27 23:26:22 -0500

    I'm starting to get kinda good at this bash thing

--- build.sh
+++ build.sh
@@ -1,9 +1,92 @@
 #!/bin/bash
 # set -xe
 
+# Use the sudo command if the user is not root
+
+project_name="ascii_video"
+author="cobyj33"
+version="0.4.0"
+github_url="https://www.github.com/cobyj33/ascii_video"
+git_repo="https://www.github.com/cobyj33/ascii_video.git"
+
+libavcodec_min_version="59.42.100"
+libavformat_min_version="59.30.100"
+libavutil_min_version="57.32.101"
+libswresample_min_version="4.8.100"
+libswscale_min_version="6.8.102"
+
+is_debian=$([[ -f "/etc/debian_version" ]])
+
+if [[ ! $is_debian -eq 0 ]]; then
+   echo "Cannot build ${project_name}. Project can only currently build \ 
+   and is only currently tested to build on Debian-Linux systems. This is in place \
+   to protect any errors that it would cause on untested architecture."
+   exit 1
+fi
+
+prompt_confirm() {
+    while true; do
+        read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+        echo ""
+        case $REPLY in
+        [yY]) echo ; return 0 ;;
+        [nN]) echo ; return 1 ;;
+        *) printf " \033[31m %s \n\033[0m" "invalid input"
+        esac 
+    done  
+}
+
+is_ncurses6_already_installed_globally() {
+    [[ ! $(ldconfig -p | grep "libncurses.so.6\|libncursesw.so.6") = \"\"  ]]
+    return $?
+}
+
+echo "Building ${project_name} by ${cobyj33}. Version ${version}. See source code at ${github_url}"
+
 SUDO=''
 if (( $EUID != 0 )); then
     SUDO='sudo'
+fi
+
+# Basic dependencies needed to build software on Debian-Type distributions 
+# These will be needed for ncurses and building ascii_video itself
+building_deps="git \
+    autoconf \
+    automake \
+    build-essential \
+    make \
+    cmake \
+    git \
+    libtool \
+    pkg-config"
+
+ffmpeg_deps="meson \
+    ninja-build \
+    pkg-config \
+    yasm \
+    libva-dev \
+    libvdpau-dev \
+    libvorbis-dev \
+    libxcb1-dev \
+    libxcb-shm0-dev \
+    libxcb-xfixes0-dev \
+    libunistring-dev \
+    libaom-dev \
+    libdav1d-dev"
+
+# Update current repository
+ncurses_apt="libncurses6"
+apt_deps_to_install="${building_deps} ${ncurses_apt}"
+
+if [[ ! -d $(pwd)/lib/build/ffmpeg ]]
+then
+    apt_deps_to_install="${apt_deps_to_install} ${ffmpeg_deps}"
+fi
+
+if [[ ! apt_deps_to_install -eq "" ]]
+then
+    $SUDO apt-get update
+    $SUDO apt-get install ${apt_deps_to_install}
 fi
 
 project_root=$(pwd)
@@ -22,110 +105,67 @@ ffmpeg_build=${lib_build}/ffmpeg-6.0
 
 ascii_video_build=${project_root}/build
 
-if [ ! -d ${lib_build} ]
+mkdir -p ${lib_build}
+
+# Defaults to local ncurses build if that is found
+should_install_ncurses6_locally=false
+
+if [[ $(is_ncurses6_already_installed_globally) -eq 0 ]]
 then
-    mkdir ${lib_build}
+    [[ ! -d ${ncurses_build} ]] && \
+    echo "Found a working ncurses6 installation already on system." && \
+    echo "Defaulting to link toward global ncurses6 installation"
+else
+    echo "Could not find a working ncurses6 installation already on system"
+
+    if [[ is_debian -eq 0 ]] && [[ $(prompt_confirm "Would you like to install ncurses6 with apt installer" ) -eq 0 ]] \
+    && $SUDO apt-get install libncurses6 || should_install_ncurses6_locally=true
 fi
 
-$SUDO apt-get update
-
-$SUDO apt-get install build-essential \
-cmake \
-make \
-autoconf \
-automake \
-libtool
-
-if [ ! -d ${ncurses_build} ] 
+if [[ should_install_ncurses6_locally = true ]]
 then
-    if [ ! -d ${ncurses_src} ]
+
+    if [[ -d ${ncurses_build} ]]
     then
+        echo "Found local ncurses6 build at ${ncurses_build}. Not recompiling ncurses6 library"
+    elif [[ ! -d ${ncurses_build} ]]
+    then
+        rm -rf ${ncurses_src}
         tar -xvf ${ncurses_tar} -C ${project_lib_folder}
+        cd ${ncurses_src}
+        mkdir -p ${ncurses_build}
+        mkdir -p ${ncurses_datadir}
+
+        ./configure \
+        --prefix=${ncurses_build} \
+        --datadir=${ncurses_datadir} \
+        --without-tests \
+        --without-manpages \
+        --without-progs
+
+
+        make -j4
+        make install
+        cd ${project_root}
+        rm -rf ${ncurses_src}
     fi
 
-    cd ${ncurses_src}
-
-    # mkdir ${project_lib_folder}/ncurses-6.4/build 
-    # mkdir ${project_lib_folder}/ncurses-6.4/build/data
-    if [ ! -d ${ncurses_build} ]
-    then
-        mkdir ${ncurses_build}
-    fi
-
-    if [ ! -d ${ncurses_datadir} ]
-    then
-        mkdir ${ncurses_datadir}
-    fi
-
-    ./configure \
-    --prefix=${ncurses_build} \
-    --datadir=${ncurses_datadir} \
-    --without-tests \
-    --without-manpages \
-    --without-progs
-
-
-    make -j4
-    make install
-    cd ${project_root}
-    rm -rf ${ncurses_src}
 fi
 
-if [ ! -d ${ffmpeg_build} ] 
-then
 
-    mkdir ${ffmpeg_build}
-    if [ ! -d ${ffmpeg_src} ]
-    then
-        tar -xvf ${ffmpeg_tar} -C ${project_lib_folder}
-    fi
+if [[ ! -d ${ffmpeg_build} ]]
+then
+    mkdir -p ${ffmpeg_build}
+    rm -rf ${ffmpeg_src}
+    tar -xvf ${ffmpeg_tar} -C ${project_lib_folder}
     cd ${ffmpeg_src}
 
     $SUDO apt-get update -qq
-
-    deps="autoconf \
-    automake \
-    build-essential \
-    cmake \
-    git \
-    libtool \
-    meson \
-    ninja-build \
-    pkg-config \
-    yasm \
-    libva-dev \
-    libvdpau-dev \
-    libvorbis-dev \
-    libxcb1-dev \
-    libxcb-shm0-dev \
-    libxcb-xfixes0-dev \
-    libunistring-dev \
-    libaom-dev \
-    libdav1d-dev"
-
-    $SUDO apt-get install -y \
-    autoconf \
-    automake \
-    build-essential \
-    cmake \
-    git \
-    libtool \
-    meson \
-    ninja-build \
-    pkg-config \
-    yasm \
-    libva-dev \
-    libvdpau-dev \
-    libvorbis-dev \
-    libxcb1-dev \
-    libxcb-shm0-dev \
-    libxcb-xfixes0-dev \
-    libunistring-dev \
-    libaom-dev \
-    libdav1d-dev
+    $SUDO apt-get install -y ${ffmpeg_deps}
 
 
     PKG_CONFIG_PATH="${ffmpeg_build}/lib/pkgconfig"
+
     ./configure \
     --prefix="${ffmpeg_build}" \
     --pkg-config-flags="--static" \
@@ -160,11 +200,7 @@ then
     cd ${project_root}
 fi
 
-if [ -d ${ascii_video_build} ]
-then
-    rm -rf ${ascii_video_build}
-fi
-
+rm -rf ${ascii_video_build}
 mkdir ${ascii_video_build}
 cd ${ascii_video_build}
 cmake ../
--- install.sh
+++ install.sh
@@ -1,50 +1,106 @@
 #!/bin/bash
 
-if [ ! -f "./build/ascii_video" ]
+project_root=$(pwd)
+
+get_local_bin_path_addition_string() {
+    echo "# set PATH so it includes user's private bin if it exists"
+    echo "if [ -d \"$1\" ] ; then"
+    echo "  PATH=\"$1:\$PATH\""
+    echo "fi"
+}
+
+prompt_confirm() {
+    while true; do
+        read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+        echo ""
+        case $REPLY in
+        [yY]) echo ; return 0 ;;
+        [nN]) echo ; return 1 ;;
+        *) printf " \033[31m %s \n\033[0m" "invalid input"
+        esac 
+    done  
+}
+
+add_local_bin_path() {
+    if [[ -f $2 ]]
+    then
+        grep PATH=\"$1:\$PATH\" $2
+        if [[ ! $? = "" ]]
+        then
+            echo "Found that $1 has already been appended to the path variable. No write done to $2"
+            return 1
+        else
+            echo "Adding $1 to path declarations in file $2"
+            get_local_bin_path_addition_string $1 | cat >> $2
+            return 0
+        fi
+    else
+        echo "Failure: Tried to add $1 to PATH in file \"$2\" . File does not exist"
+        return 2
+    fi
+}
+
+previously_installed_location="$(whereis ascii_video | sed "s/ascii_video:[ ]*//")"
+
+if [[ ! ${previously_installed_location} = "" ]]
+then
+    prompt_confirm "ascii_video detected to be installed at $previously_installed_location. Would you like to overwrite?"
+    if [[ $? -eq 0 ]] 
+    then
+        echo "Set to overwrite ascii_video installation at $previously_installed_location"
+    else
+        echo "Will not overwrite ascii_video installation at $previously_installed_location."
+        echo "Exiting installation script for ascii_video"
+        exit 0
+    fi
+fi
+
+if [[ ! -f $project_root/build/ascii_video ]]
 then
     echo "Cannot install ascii_video binary, as the ascii_video binary does not currently exist. Run 'chmod +x ./build.sh && ./build.sh' to build the binary"
     exit 1
 fi
 
-is_root=true
-install_location="~/.local/bin"
+global_install_location="/usr/local/bin"
+user_install_location="$HOME/.local/bin"
 
-if (( $EUID != 0 )); then
-    is_root=false
-    install_location="/usr/local/bin"
+
+[[ $EUID -eq 0 ]]
+is_root=$?
+install_location=$([[ $is_root -eq 0 ]] && echo $global_install_location || echo $user_install_location)
+
+if [[ is_root -eq false ]] && [[ ! -d $user_install_location ]]
+then
+    echo "$user_install_location could not be found. Creating $user_install_location"
+    mkdir -p $user_install_location
 fi
 
-if [ is_root -eq false ]
+if [[ is_root -eq false ]]
 then
+    shell_rc="$HOME/.$( echo $SHELL | sed -nE "s|.*/(.*)\$|\1|p" )rc"
 
-    if [ ! -d ~/.local/bin ]
+    if [[ -f ${shell_rc} ]]
     then
-        echo "~/.local could not be found. Creating ~/.local"
-        mkdir ~/.local
-    fi
-
-    if [ ! -d ~/.local/bin ]
+        add_local_bin_path $user_install_location ${shell_rc}
+    elif [[ -f $HOME/.bash_profile ]]
     then
-        echo "~/.local/bin could not be found. Creating ~/.local/bin"
-        mkdir ~/.local/bin
-        shell_rc=${"~/.$(echo $SHELL | sed -nE "s|.*/(.*)\$|\1|p")rc"}
-
-        if [ ! -e ${shell_rc}]
-        then   
-            echo "Could not add ~/.local/bin to path, could not find shell rc file to modify to modify path"
-        else
-            echo "Adding ~/.local/bin to ${shell_rc}"
-            echo "export $PATH=$HOME/.local/bin:$PATH" >> ${shell_rc}
-        fi
+        add_local_bin_path $user_install_location $HOME/.bash_profile
+    elif [[ -f $HOME/.bash_login ]]
+    then
+        add_local_bin_path $user_install_location $HOME/.bash_login
+    elif [[ -f $HOME/.profile ]]
+    then
+        add_local_bin_path $user_install_location $HOME/.profile
+    else
+        echo "Could not add $user_install_location, could not find proper shell configuration file to modify path"
     fi
 
 fi
 
-
-if [ -f "./build/ascii_video" ]
+if [[ -f $project_root/build/ascii_video ]]
 then
-    echo "Installing ascii_video binary to ${install_location}"
-    cp -R ./build/ascii_video ${install_location}
+    echo "Installing ascii_video binary to $install_location"
+    cp -R $project_root/build/ascii_video $install_location
     hash -r
 else
     echo "Cannot install ascii_video binary, as the ascii_video binary does not currently exist. Run 'chmod +x ./build.sh && ./build.sh' to build the binary"
