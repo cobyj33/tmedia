37aefa93b190291274a3ce523e3fce57e7248799
cobyj33 <jacobyajohnson@gmail.com>
2023-09-27 01:52:19 -0500

    Should I do this?

---
+++ include/playback.h
@@ -0,0 +1,18 @@
+#ifndef ASCII_VIDEO_PLAYBACK_H
+#define ASCII_VIDEO_PLAYBACK_H
+
+#include "media.h"
+
+class MediaPlaybackContext {
+  MediaPlayer media;
+  MediaClock clock;
+  MediaGUI guiSettings;
+
+  MediaPlaybackContext(MediaPlayer media);
+}
+
+void asv_player_set_media();
+
+void asv_player_remove_current_media();
+
+#endif
--- include/renderer.h
+++
@@ -1,8 +0,0 @@
-#ifndef ASCII_VIDEO_RENDERER_H
-#define ASCII_VIDEO_RENDERER_H
-
-void renderer_add_media_player();
-
-void renderer_remove_media_player();
-
-#endif
