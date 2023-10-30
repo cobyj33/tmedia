e76869b3af3a54ebcf13a182127b9f01568aa761
cobyj33 <jacobyajohnson@gmail.com>
2023-10-29 20:42:51 -0500

    Update media_type_from_avformat_context to use sets

--- src/mediatype.cpp
+++ src/mediatype.cpp
@@ -5,6 +5,8 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <set>
+#include <string_view>
 
 extern "C" {
   #include <libavformat/avformat.h>
@@ -19,19 +21,33 @@ std::string media_type_to_string(MediaType media_type) {
   throw std::runtime_error("Attempted to get Media Type string from unimplemented Media Type. Please implement all MediaType's to return a valid string. This is a developer error and bug.");
 }
 
+/**
+ * Note that for media_type_from_avformat_context, the recognized iformat names
+ * can only be used to correctly identify media file formats that have one specific
+ * file type. Things like mp4 and webm are more so containers and really can't be
+ * guaranteed to be identifiable from a format name.
+ * 
+ * Also, note that the iformat->name can be a comma separated list of names for
+ * different media file types. This isn't implemented here, as the media types
+ * here should all just be the given name, but it would be of note if trying
+ * to implement new file formats.
+*/
+
 MediaType media_type_from_avformat_context(AVFormatContext* format_context) {
-  std::vector<std::string> image_formats{"image2", "png_pipe", "webp_pipe"};
-  std::vector<std::string> audio_formats{"wav", "ogg", "mp3", "flac"};
-  for (std::size_t i = 0; i < image_formats.size(); i++) {
-    if (strcmp(format_context->iformat->name, image_formats[i].c_str()) == 0) {
-      return MediaType::IMAGE;
-    }
+  static const std::set<std::string_view> image_iformat_names{"image2", "png_pipe", "webp_pipe"};
+  static const std::set<std::string_view> audio_iformat_names{"wav", "ogg", "mp3", "flac"};
+  static const std::set<std::string_view> video_iformat_names{"flv"};
+
+  if (image_iformat_names.count(format_context->iformat->name) == 1) {
+    return MediaType::IMAGE;
+  } 
+
+  if (audio_iformat_names.count(format_context->iformat->name) == 1) {
+    return MediaType::AUDIO;
   }
 
-  for (std::size_t i = 0; i < audio_formats.size(); i++) {
-    if (strcmp(format_context->iformat->name, audio_formats[i].c_str()) == 0) {
-      return MediaType::AUDIO;
-    }
+  if (video_iformat_names.count(format_context->iformat->name) == 1) {
+    return MediaType::VIDEO;
   }
 
   if (avformat_context_has_media_stream(format_context, AVMEDIA_TYPE_VIDEO)) {
