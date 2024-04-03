f71b932820557b835f51eb63642419344da052a1
cobyj33 <jacobyajohnson@gmail.com>
2024-04-03 17:14:54 -0500

    Actually, I'ma do some forward declares

--- src/audioresampler.cpp
+++ src/audioresampler.cpp
@@ -2,7 +2,9 @@
 
 #include "avguard.h"
 #include "ffmpeg_error.h"
+#include "funcmac.h"
 
+#include <fmt/format.h>
 #include <stdexcept>
 
 extern "C" {
@@ -34,16 +36,19 @@ AudioResampler::AudioResampler(int64_t dst_ch_layout, enum AVSampleFormat dst_sa
     if (context != nullptr) {
       swr_free(&context);
     }
-    throw ffmpeg_error("[AudioResampler::AudioResampler] Allocation of internal SwrContext of AudioResampler failed. Aborting...", result);
+    throw ffmpeg_error(fmt::format("[{}] Allocation of internal SwrContext of "
+    "AudioResampler failed. Aborting...", FUNCDINFO), result);
   } else if (context == nullptr) {
-    throw std::runtime_error("[AudioResampler::AudioResampler] Allocation of internal SwrContext of AudioResampler failed. Aborting...");
+    throw std::runtime_error(fmt::format("[{}] Allocation of internal "
+    "SwrContext of AudioResampler failed. Aborting...", FUNCDINFO));
   } else {
     result = swr_init(context);
     if (result < 0) {
       if (context != nullptr) {
         swr_free(&context);
       }
-      throw ffmpeg_error("[AudioResampler::AudioResampler] Initialization of internal SwrContext of AudioResampler failed. Aborting...", result);
+      throw ffmpeg_error(fmt::format("[{}] Initialization of internal "
+      "SwrContext of AudioResampler failed. Aborting...", FUNCDINFO), result);
     }
   }
 
