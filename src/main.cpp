3299d65645740bbfaa10e9e6ff36a56c927831c8
cobyj33 <jacobyajohnson@gmail.com>
2024-05-30 22:56:47 -0500

    newdelwatch multithreading mutex locking

--- src/main.cpp
+++ src/main.cpp
@@ -43,45 +43,70 @@ void on_terminate() {
 # define NEWDELTRACKER_AT_OR_ABOVE_CXX17 1
 #endif
 
+// Uncomment line below to define NEWDELTRACKER_MULTITHREADED for mutexes
+#define NEWDELTRACKER_MULTITHREADED
+
+#if defined(NEWDELTRACKER_MULTITHREADED)
+#include <mutex>
+#endif
+
+
 namespace newdelwatch {
-  std::size_t new_call_count = 0;
-  std::size_t new_array_call_count = 0;
-
-  std::size_t delete_call_count = 0;
-  std::size_t delete_sized_call_count = 0;
-  std::size_t delete_array_call_count = 0;
-  std::size_t delete_sized_array_call_count = 0;
-
-  #if defined(NEWDELTRACKER_AT_OR_ABOVE_CXX17)
-  std::size_t new_align_call_count = 0;
-  std::size_t new_array_align_call_count = 0;
-
-  std::size_t delete_align_call_count = 0;
-  std::size_t delete_sized_align_call_count = 0;
-  std::size_t delete_array_align_call_count = 0;
-  std::size_t delete_sized_array_align_call_count = 0;
-  #endif
 
   void reset();
   void report(std::ostream& ostream);
+  void* generic_new(const std::size_t sz);
+  void generic_free(void* ptr);
+
+  #if defined(NEWDELTRACKER_MULTITHREADED)
+  std::mutex new_mutex; // only to be locked by generic_new
+  std::mutex del_mutex; // only to be locked by generic_free
+  #endif
+
+  struct Counter {
+    std::size_t nb;
+    #if defined(NEWDELTRACKER_MULTITHREADED)
+    std::mutex mutex;
+    #endif
+    Counter() = default;
+    Counter(std::size_t _nb) : nb(_nb) {}
+  };
+
+  Counter new_call_count = 0;
+  Counter new_array_call_count = 0;
+
+  Counter delete_call_count = 0;
+  Counter delete_sized_call_count = 0;
+  Counter delete_array_call_count = 0;
+  Counter delete_sized_array_call_count = 0;
+
+  #if defined(NEWDELTRACKER_AT_OR_ABOVE_CXX17)
+  Counter new_align_call_count = 0;
+  Counter new_array_align_call_count = 0;
+
+  Counter delete_align_call_count = 0;
+  Counter delete_sized_align_call_count = 0;
+  Counter delete_array_align_call_count = 0;
+  Counter delete_sized_array_align_call_count = 0;
+  #endif
 
   void reset() {
-    new_call_count = 0;
-    new_array_call_count = 0;
+    new_call_count.nb = 0;
+    new_array_call_count.nb = 0;
 
-    delete_call_count = 0;
-    delete_sized_call_count = 0;
-    delete_array_call_count = 0;
-    delete_sized_array_call_count = 0;
+    delete_call_count.nb = 0;
+    delete_sized_call_count.nb = 0;
+    delete_array_call_count.nb = 0;
+    delete_sized_array_call_count.nb = 0;
 
     #if defined(NEWDELTRACKER_AT_OR_ABOVE_CXX17) || _MSVC_LANG >= 201703L
-    new_align_call_count = 0;
-    new_array_align_call_count = 0;
+    new_align_call_count.nb = 0;
+    new_array_align_call_count.nb = 0;
 
-    delete_align_call_count = 0;
-    delete_sized_align_call_count = 0;
-    delete_array_align_call_count = 0;
-    delete_sized_array_align_call_count = 0;
+    delete_align_call_count.nb = 0;
+    delete_sized_align_call_count.nb = 0;
+    delete_array_align_call_count.nb = 0;
+    delete_sized_array_align_call_count.nb = 0;
     #endif
   }
 
@@ -90,69 +115,86 @@ namespace newdelwatch {
    * @param ostream The output stream to print character data to for printing
   */
   void report(std::ostream& ostream) {
-    ostream << "\t" << new_call_count << " calls to new tracked" << std::endl;
-    ostream << "\t" << new_array_call_count << " calls to new[] tracked" << std::endl;
+    ostream << "\t" << new_call_count.nb << " calls to new tracked" << std::endl;
+    ostream << "\t" << new_array_call_count.nb << " calls to new[] tracked" << std::endl;
 
-    ostream << "\t" << delete_call_count << " calls to delete(void*) tracked" << std::endl;
-    ostream << "\t" << delete_sized_call_count << " calls to delete(void*, std::size_t) tracked" << std::endl;
-    ostream << "\t" << delete_array_call_count << " calls to delete[](void*) tracked" << std::endl;
-    ostream << "\t" << delete_sized_array_call_count << " calls to delete[](void*, std::size_t) tracked" << std::endl;
+    ostream << "\t" << delete_call_count.nb << " calls to delete(void*) tracked" << std::endl;
+    ostream << "\t" << delete_sized_call_count.nb << " calls to delete(void*, std::size_t) tracked" << std::endl;
+    ostream << "\t" << delete_array_call_count.nb << " calls to delete[](void*) tracked" << std::endl;
+    ostream << "\t" << delete_sized_array_call_count.nb << " calls to delete[](void*, std::size_t) tracked" << std::endl;
 
 
     #if defined(NEWDELTRACKER_AT_OR_ABOVE_CXX17)
-    ostream << "\t" << new_align_call_count << " calls to aligned new tracked" << std::endl;
-    ostream << "\t" << new_array_align_call_count << " calls to aligned new[] tracked" << std::endl;
+    ostream << "\t" << new_align_call_count.nb << " calls to aligned new tracked" << std::endl;
+    ostream << "\t" << new_array_align_call_count.nb << " calls to aligned new[] tracked" << std::endl;
 
-    ostream << "\t" << delete_align_call_count << " calls to aligned delete(void*) tracked" << std::endl;
-    ostream << "\t" << delete_sized_align_call_count << " calls to aligned delete(void*, std::size_t) tracked" << std::endl;
-    ostream << "\t" << delete_array_align_call_count << " calls to aligned delete[](void*) tracked" << std::endl;
-    ostream << "\t" << delete_sized_array_align_call_count << " calls to aligned delete[](void*, std::size_t) tracked" << std::endl;
+    ostream << "\t" << delete_align_call_count.nb << " calls to aligned delete(void*) tracked" << std::endl;
+    ostream << "\t" << delete_sized_align_call_count.nb << " calls to aligned delete(void*, std::size_t) tracked" << std::endl;
+    ostream << "\t" << delete_array_align_call_count.nb << " calls to aligned delete[](void*) tracked" << std::endl;
+    ostream << "\t" << delete_sized_array_align_call_count.nb << " calls to aligned delete[](void*, std::size_t) tracked" << std::endl;
     #endif
   }
-}
 
-void* generic_new(const std::size_t sz) {
-  void *ptr = std::malloc(sz);
-  if (ptr) {
-    return ptr;
-  } else {
-    throw std::bad_alloc{};
+  void* generic_new(const std::size_t sz) {
+    #if defined(NEWDELTRACKER_MULTITHREADED)
+    std::lock_guard<std::mutex> lock(new_mutex);
+    #endif
+    void *ptr = std::malloc(sz);
+    if (ptr) {
+      return ptr;
+    } else {
+      throw std::bad_alloc{};
+    }
   }
-}
+
+  void generic_free(void* ptr) {
+    #if defined(NEWDELTRACKER_MULTITHREADED)
+    std::lock_guard<std::mutex> lock(del_mutex);
+    #endif
+    std::free(ptr);
+  }
+
+  void increment(newdelwatch::Counter& counter) {
+    #if defined(NEWDELWATCH_MULTITHREADED)
+    std::lock_guard<std::mutex> lock(counter.mutex);
+    #endif
+    counter.nb++;
+  }
+} // namespace newdelwatch
 
 // Unaligned New and Delete New Overrides
 
 void* operator new(const std::size_t sz) {
-  newdelwatch::new_call_count++;
-	return generic_new(sz);
+	newdelwatch::increment(newdelwatch::new_call_count);
+	return newdelwatch::generic_new(sz);
 }
 
 void* operator new[](const std::size_t sz) {
-  newdelwatch::new_array_call_count++;
-	return generic_new(sz);
+	newdelwatch::increment(newdelwatch::new_array_call_count);
+	return newdelwatch::generic_new(sz);
 }
 
 // Delete Overrides
 
 void operator delete(void * ptr) noexcept(true) {
-  newdelwatch::delete_call_count++;
-  std::free(ptr);
+	newdelwatch::increment(newdelwatch::delete_call_count);
+  newdelwatch::generic_free(ptr);
 }
 
 void operator delete(void * ptr, std::size_t sz) noexcept(true) {
-  newdelwatch::delete_sized_call_count++;
-  std::free(ptr);
+	newdelwatch::increment(newdelwatch::delete_sized_call_count);
+  newdelwatch::generic_free(ptr);
   (void)sz;
 }
 
 void operator delete[](void* ptr) noexcept{
-  newdelwatch::delete_array_call_count++;
-	std::free(ptr);
+	newdelwatch::increment(newdelwatch::delete_array_call_count);
+	newdelwatch::generic_free(ptr);
 }
 
 void operator delete[](void* ptr, std::size_t sz) noexcept{
-  newdelwatch::delete_sized_array_call_count++;
-	std::free(ptr);
+	newdelwatch::increment(newdelwatch::delete_sized_array_call_count);
+	newdelwatch::generic_free(ptr);
   (void)sz;
 }
 
@@ -165,39 +207,39 @@ void operator delete[](void* ptr, std::size_t sz) noexcept{
 // This'll probably break something because its not actually
 // aligning anything in memory... but whatever we'll see
 void* operator new(const std::size_t sz, std::align_val_t al) {
-  newdelwatch::new_align_call_count++;
-	return generic_new(sz);
+	newdelwatch::increment(newdelwatch::new_align_call_count);
+	return newdelwatch::generic_new(sz);
   (void)al;
 }
 
 void* operator new[](const std::size_t sz, std::align_val_t al) {
-  newdelwatch::new_array_align_call_count++;
-	return generic_new(sz);
+	newdelwatch::increment(newdelwatch::new_array_align_call_count);
+	return newdelwatch::generic_new(sz);
   (void)al;
 }
 
 void operator delete(void * ptr, std::align_val_t al) noexcept(true) {
-  newdelwatch::delete_align_call_count++;
-  std::free(ptr);
+	newdelwatch::increment(newdelwatch::delete_align_call_count);
+  newdelwatch::generic_free(ptr);
   (void)al;
 }
 
 void operator delete(void * ptr, std::size_t sz, std::align_val_t al) noexcept(true) {
-  newdelwatch::delete_sized_align_call_count++;
-  std::free(ptr);
+	newdelwatch::increment(newdelwatch::delete_sized_align_call_count);
+  newdelwatch::generic_free(ptr);
   (void)sz;
   (void)al;
 }
 
 void operator delete[](void* ptr, std::align_val_t al) noexcept{
-  newdelwatch::delete_array_align_call_count++;
-	std::free(ptr);
+	newdelwatch::increment(newdelwatch::delete_array_align_call_count);
+	newdelwatch::generic_free(ptr);
   (void)al;
 }
 
 void operator delete[](void* ptr, std::size_t sz, std::align_val_t al) noexcept{
-  newdelwatch::delete_sized_array_align_call_count++;
-	std::free(ptr);
+	newdelwatch::increment(newdelwatch::delete_sized_array_align_call_count);
+	newdelwatch::generic_free(ptr);
   (void)sz;
   (void)al;
 }
@@ -229,6 +271,7 @@ int main(int argc, char** argv) {
 
   TMediaCLIParseRes res = tmedia_parse_cli(argc, argv);
   if (res.exited) {
+    newdelwatch::report(std::cout);
     return EXIT_SUCCESS;
   }
   
@@ -237,11 +280,12 @@ int main(int argc, char** argv) {
     for (const std::string& err : res.errors) {
       std::cerr << "  " <<  err << std::endl;
     }
+    newdelwatch::report(std::cout);
     return EXIT_FAILURE;
   }
 
+  newdelwatch::reset();
   int exitcode = tmedia_run(res.tmss);
   newdelwatch::report(std::cout);
-  newdelwatch::reset();
   return exitcode;
 }
