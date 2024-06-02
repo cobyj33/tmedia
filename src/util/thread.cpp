c6ccadd4bbc7f010a603490fc6438ad6309d2475
cobyj33 <jacobyajohnson@gmail.com>
2024-06-02 14:59:07 -0500

    Some name_current_thread doc comments

--- src/util/thread.cpp
+++ src/util/thread.cpp
@@ -9,8 +9,21 @@
 #endif
 
 
+// Since there is no C++ 11 supported cross-platform way
+// to give a current thread a name viewable by other
+// programs on the system, we have to handle each platform
+// in a specific way
 void name_current_thread(std::string_view name) {
   #if defined(__linux__)
+  // While we could use pthreads' pthread_setname_np function
+  // to set the current thread name on a thread's pthread_t
+  // handle, these functions are actually nonstandard GNU
+  // extensions, and aren't guaranteed to be a part of
+  // pthread.h anyway. Additionally, it would require a
+  // direct linking to pthread in the build system to find
+  // and link to libpthread, which isn't necessary when we can just call
+  // the system directly and libpthread may not even have the extension
+  // anyway.
   prctl(PR_SET_NAME, name.data(), 0, 0, 0);
   #endif
 }
